e6c5f26fcecf55c35bdc3f84a6debd6e32805e33
IlhanTech <ilhan.neuville@epitech.eu>
2023-05-23 15:04:40 +0200

    update

--- get_road.sh
+++ get_road.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 
 url=$1
+output_file="output.txt"
 
-dirb_recursive() {
-    while true; do
-        output=$(dirb "$url" /usr/share/dirb/wordlists/common.txt -X .html -r)
+dir_recursive() {
+    local url="$1"
+    local indent="$2"
 
-        # Vérifier si des dossiers ont été trouvés dans la sortie de Dirb
-        if grep -q "DIRECTORY: " <<< "$output"; then
-            # Extraire les noms de dossiers trouvés
-            folders=$(grep "DIRECTORY: " <<< "$output" | cut -d ' ' -f 2-)
+    echo "Exploration de $url"
 
-            # Parcourir chaque dossier trouvé
-            while IFS= read -r folder; do
-                # Effectuer une action supplémentaire avec chaque dossier
-                echo "Dossier trouvé : $folder"
-                # Récursion pour explorer le sous-dossier
-                dirb_recursive "$url/$folder"
-            done <<< "$folders"
-        else
-            # Aucun dossier trouvé, sortir de la boucle
-            break
-        fi
+    wget --spider --no-check-certificate --recursive --level=1 --no-parent "$url" 2>&1 | grep -oP 'https?://\K[^ ]+' | grep -v 'index\.\(html\|php\)$' | awk '{print "'$indent'" $0}' >> "$output_file"
+
+    local sub_urls=$(grep -oP 'https?://\K[^ ]+' "$output_file")
+    for sub_url in $sub_urls; do
+        dir_recursive "$sub_url" "$indent  "
     done
 }
 
@@ -31,7 +23,10 @@ if [ -z "$url" ]; then
     exit 1
 fi
 
-# Exécuter la fonction dirb_recursive et rediriger la sortie vers le fichier output.txt
-dirb_recursive > output.txt
+# Supprimer le fichier de sortie s'il existe déjà
+rm -f "$output_file"
 
-echo "Exploration terminée. Les résultats ont été enregistrés dans output.txt."
+# Exécuter la fonction dir_recursive
+dir_recursive "$url" ""
+
+echo "Exploration terminée. Les résultats ont été enregistrés dans $output_file."
