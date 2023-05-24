05f6f5a99f4c8655e079a885df847fe7b2d3df75
IlhanTech <ilhan.neuville@epitech.eu>
2023-05-24 12:20:53 +0200

    update

--- get_road.sh
+++ get_road.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
 
-url=$1
-output_file="output.txt"
+base_url="http://10.10.166.131"
+wordlist="/usr/share/dirb/wordlists/common.txt"
 
-dir_recursive() {
-    local url="$1"
-    local indent="$2"
+while true; do
+  # Exécuter la commande dirb
+  output=$(dirb "$base_url" -w "$wordlist" -q)
 
-    echo "Exploration de $url"
+  # Extraire les chemins des dossiers trouvés
+  paths=$(echo "$output" | grep -oP '(?<=\+ )/\S+')
 
-    wget --spider --no-check-certificate --recursive --level=1 --no-parent "$url" 2>&1 | grep -oP 'https?://\K[^ ]+' | grep -v 'index\.\(html\|php\)$' | awk '{print "'$indent'" $0}' >> "$output_file"
+  # Vérifier s'il y a des dossiers trouvés
+  if [ -z "$paths" ]; then
+    echo "Aucun dossier trouvé. Arrêt du script."
+    break
+  fi
 
-    local sub_urls=$(grep -oP 'https?://\K[^ ]+' "$output_file")
-    for sub_url in $sub_urls; do
-        dir_recursive "$sub_url" "$indent  "
-    done
-}
+  # Afficher les dossiers trouvés
+  echo "Dossiers trouvés :"
+  echo "$paths"
 
-# Vérifier si l'adresse IP a été fournie en argument
-if [ -z "$url" ]; then
-    echo "Veuillez fournir l'adresse IP en argument."
-    exit 1
-fi
+  # Mettre à jour la base URL pour les itérations suivantes
+  base_url="$base_url${paths%/*}/"
 
-# Supprimer le fichier de sortie s'il existe déjà
-rm -f "$output_file"
-
-# Exécuter la fonction dir_recursive
-dir_recursive "$url" ""
-
-echo "Exploration terminée. Les résultats ont été enregistrés dans $output_file."
+  sleep 1  # Attendre 1 seconde entre chaque itération
+done
