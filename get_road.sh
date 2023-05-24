9e4e1b88a4ab45b5f615a24146de1667c96fa3eb
IlhanTech <ilhan.neuville@epitech.eu>
2023-05-24 12:55:55 +0200

    update

--- get_road.sh
+++ get_road.sh
@@ -3,23 +3,53 @@
 base_url="http://10.10.166.131"
 wordlist="/usr/share/dirb/wordlists/common.txt"
 
-# Exécuter la commande dirb
-output=$(dirb "$base_url" -w "$wordlist")
+# Fonction récursive pour explorer les dossiers
+explore_folders() {
+  local url="$1"
+  local current_path="$2"
 
-while true; do
-  # Extraire le dernier dossier trouvé par dirb
-  folder=$(echo "$output" | grep -oP '(?<=\+ )[^ ]+(?=/$)' | tail -1)
+  # Exécuter dirb pour le dossier actuel
+  output=$(dirb "$url" "$wordlist")
 
-  if [ -n "$folder" ]; then
-    # Concaténer l'URL avec le dossier trouvé
-    url="$base_url/$folder"
+  # Extraire les nouveaux dossiers trouvés
+  folders=$(echo "$output" | grep -oP '(?<=\+ )[^ ]+(?=/)')
 
-    # Relancer dirb pour explorer le nouveau dossier
-    output=$(dirb "$url" -w "$wordlist")
-  else
-    # Aucun dossier trouvé, afficher le dernier URL
-    last_url=$(echo "$output" | grep -oP '(?<=+ )[^ ]+' | tail -1)
-    echo "Dernier URL : $last_url"
-    break
-  fi
-done
+  # Parcourir les dossiers et explorer récursivement
+  for folder in $folders; do
+    # Construire le nouvel URL pour le dossier
+    new_url="$url/$folder/"
+
+    # Construire le nouveau chemin de fichier pour le dossier
+    new_path="$current_path/$folder"
+
+    # Afficher le contenu du dossier
+    echo "Contenu du dossier $new_url :"
+    cat "$new_path"
+
+    # Explorer récursivement les sous-dossiers
+    explore_folders "$new_url" "$new_path"
+  done
+}
+
+# Exécuter la commande dirb initiale
+output=$(dirb "$base_url" "$wordlist")
+
+# Extraire le dernier dossier trouvé par dirb
+folder=$(echo "$output" | grep -oP '(?<=\+ )[^ ]+(?=/)' | tail -1)
+
+if [ -n "$folder" ]; then
+  # Construire l'URL et le chemin de fichier pour le dossier initial
+  url="$base_url/$folder/"
+  path="$folder"
+
+  # Afficher le contenu du dossier initial
+  echo "Contenu du dossier $url :"
+  cat "$path"
+
+  # Explorer récursivement les sous-dossiers
+  explore_folders "$url" "$path"
+else
+  # Aucun dossier trouvé, afficher le dernier URL
+  last_url=$(echo "$output" | grep -oP '(?<=+ )[^ ]+' | tail -1)
+  echo "Dernier URL : $last_url"
+fi
