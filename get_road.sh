ebf1f07c90a2dc035e2720fa0cd35c8415b671d0
IlhanTech <ilhan.neuville@epitech.eu>
2023-05-24 12:23:54 +0200

    update

--- get_road.sh
+++ get_road.sh
@@ -3,25 +3,5 @@
 base_url="http://10.10.166.131"
 wordlist="/usr/share/dirb/wordlists/common.txt"
 
-while true; do
-  # Exécuter la commande dirb
-  output=$(dirb "$base_url" -w "$wordlist" -q)
-
-  # Extraire les chemins des dossiers trouvés
-  paths=$(echo "$output" | grep -oP '(?<=\+ )/\S+')
-
-  # Vérifier s'il y a des dossiers trouvés
-  if [ -z "$paths" ]; then
-    echo "Aucun dossier trouvé. Arrêt du script."
-    break
-  fi
-
-  # Afficher les dossiers trouvés
-  echo "Dossiers trouvés :"
-  echo "$paths"
-
-  # Mettre à jour la base URL pour les itérations suivantes
-  base_url="$base_url${paths%/*}/"
-
-  sleep 1  # Attendre 1 seconde entre chaque itération
-done
+# Exécuter la commande dirb pour une seule itération
+dirb "$base_url" -w "$wordlist" -q
