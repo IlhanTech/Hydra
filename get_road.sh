438958768d053a4e2d6f9511b04abc39eb8ecc68
IlhanTech <ilhan.neuville@epitech.eu>
2023-05-24 12:36:32 +0200

    update

--- get_road.sh
+++ get_road.sh
@@ -3,6 +3,11 @@
 base_url="http://10.10.166.131"
 wordlist="/usr/share/dirb/wordlists/common.txt"
 
+#!/bin/bash
+
+base_url="http://ip_adresse/"
+wordlist="common.txt"
+
 # Exécuter la commande dirb
 output=$(dirb "$base_url" -w "$wordlist")
 
@@ -11,14 +16,13 @@ while true; do
   folder=$(echo "$output" | grep -oP '(?<=\+ )[^ ]+(?=/$)' | tail -1)
 
   if [ -n "$folder" ]; then
-    # Afficher le contenu du dossier
-    echo "Contenu du dossier $folder :"
-    cat "$folder"
+    # Concaténer l'URL avec le dossier trouvé
+    url="$base_url/$folder"
 
     # Relancer dirb pour explorer le nouveau dossier
-    output=$(dirb "$base_url/$folder" -w "$wordlist")
+    output=$(dirb "$url" -w "$wordlist")
   else
-    # Aucun dossier trouvé, retourner le dernier URL
+    # Aucun dossier trouvé, afficher le dernier URL
     last_url=$(echo "$output" | grep -oP '(?<=\+ )[^ ]+(?=/)' | tail -1)
     echo "Dernier URL : $last_url"
     break
